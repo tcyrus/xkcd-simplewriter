75fb6a33540cccd5c95f5062655a48dcc262af40
TCYRUS <tcyrus@users.noreply.github.com>
2015-11-23 22:01:09 -0500

    Optimization

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
+const fs = require('fs');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
-const ipcMain = electron.ipcMain;
+const dialog = electron.dialog;
+const Menu = electron.Menu;
+const MenuItem = electron.MenuItem;
 
 // Report crashes to our server.
 electron.crashReporter.start();
@@ -16,8 +19,7 @@ function createMainWindow() {
     // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
-    title: 'simplewriter'
+    height: 600
   });
 
   // and load the index.html of the app.
@@ -30,8 +32,146 @@ function createMainWindow() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-
   return win;
+}
+
+function setMenu() {
+  let template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open..',
+          accelerator: 'CmdOrCtrl+O',
+          click(item, focusedWindow) {
+            dialog.showOpenDialog(fileNames => {
+              if(fileNames === undefined) return;
+              let fileName = fileNames[0];
+              fs.readFile(fileName, 'utf-8', (err, data) => {
+                mainWindow.webContents.send('setText', data);
+              });
+            });
+          }
+        },
+        {
+          label: 'Save As..',
+          accelerator: 'CmdOrCtrl+S',
+          click(item, focusedWindow) {
+            dialog.showSaveDialog(fileName => {
+              if(fileName === undefined) return;
+              let arg = mainWindow.webContents.send('getText', '');
+              fs.writeFile(fileName, arg, err => {});
+            });
+          }
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click(item, focusedWindow) {
+            if(focusedWindow) focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (() => (process.platform == 'darwin') ? 'Ctrl+Command+F' : 'F11')(),
+          click(item, focusedWin) {
+            if(focusedWin) focusedWin.setFullScreen(!focusedWin.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (() => (process.platform == 'darwin') ? 'Alt+Command+I' : 'Ctrl+Shift+I')(),
+          click(item, focusedWindow) {
+            if(focusedWindow) focusedWindow.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            electron.shell.openExternal('http://electron.atom.io');
+          }
+        },
+      ]
+    },
+  ];
+
+  if (process.platform == 'darwin') {
+    let name = app.getName();
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: `About ${name}`,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: `Hide ${name}`,
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click() { app.quit(); }
+        },
+      ]
+    });
+    // Window menu.
+    template[3].submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    );
+  }
+
+  let menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
 
 // Quit when all windows are closed.
@@ -51,4 +191,5 @@ app.on('activate', () => {
 
 app.on('ready', () => {
   mainWindow = createMainWindow();
+  setMenu();
 });
